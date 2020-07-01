a2e38115c19ba4ec60d8360d603947f9d72f9e80
unknown <matifuentes2@gmail.com>
2020-07-01 11:48:36 -0400

    first commit

---
+++ R/backslasher.R
@@ -0,0 +1,16 @@
+#' Backslasher Function
+#'
+#' This function allows you to replace the backslashes of the string in your clipboard by normal slashes
+#' @keywords backslash
+#' @export
+#' @examples
+
+
+backslasher <- function(){
+
+  x <- readClipboard()
+  y <- gsub("\\\\", "/", x)
+  return(y)
+}
+
+backslasher()
