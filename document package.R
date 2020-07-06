e50b01f87791cb08580a3c42d18fc57e0f529d87
unknown <matifuentes2@gmail.com>
2020-07-06 17:18:24 -0400

    delete trash script

--- document package.R
+++
@@ -1,7 +0,0 @@
-setwd("C:/Users/Matias Fuentes/Desktop/projects/PArankmi")
-devtools::document()
-setwd("..")
-devtools::install("PArankmi")
-
-#devtools::install_git("https://github.com/matifuentes2/PArankmi")
-
