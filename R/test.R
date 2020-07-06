dbd0475dff1e5e6dbe5c58f5d1f7fd8535193888
unknown <matifuentes2@gmail.com>
2020-07-06 11:38:46 -0400

    probando rproject

---
+++ R/test.R
@@ -0,0 +1,24 @@
+#' lol Function
+#'
+#' Moasdasd distribution of the ninebox with the specified boundaries
+#'
+#' @param xd Output of the df_ninbox function.
+#' @param xd2 Upper boundary for desempeño axis as dbl.
+#' @param xd3 Lower boundary for desempeño axis as dbl.
+#' @param upper_potencial Upper boundary for potencial axis as dbl.
+#' @param lower_potencial Lower boundary for potencial axis as dbl.
+#' @param col_desempeno Name of the column containing desempeño as string.
+#' @param col_potencial Name of the column containing potencial as string.
+#' @param transparency Manipulates the alpha parameter from ggplots geom_point.
+#' @examples
+#' ninebox(test, 90, 85, 100, 85, "desempeño ranco", "potencial ranco")
+#' ninebox(test2, 3, 1.5, 2, 1.5)
+#' ninebox(test3, 4, 2, 4, 2, "evaluación de desempeño", "evaluación de potencial", 0.1)
+#' @export
+
+
+test <- function (df_list, upper_desempeno, lower_desempeno, upper_potencial, lower_potencial, col_desempeno = "desempeño" , col_potencial = "potencial", transparency = 0.2){
+
+ xd
+
+}
---
+++ document package.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/Matias Fuentes/Desktop/projects/PArankmi")
+devtools::document()
+setwd("..")
+devtools::install("PArankmi")
+
+
