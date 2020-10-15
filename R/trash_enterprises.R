605a102217c0fa52ffe024353aded45cdb75dfda
matifuentes2 <matifuentes2@gmail.com>
2020-10-15 16:56:10 -0300

    Agregué función que devuelve vector con nombres de empresas de prueba

---
+++ R/trash_enterprises.R
@@ -0,0 +1,11 @@
+#' trash_enterprises Function
+#'
+#' Returns shitty enterprises from Rankmi Database (MANUELITA QUË ES ESA WEÁ BRO)
+#' @export
+
+trash_enterprises <- function(){
+
+  enterprises <- c("Acme Corp", "Avengers","CompuMundo Hiper Mega Red", "Wayne Enterprises","Empresa de prueba", "EmpresaPrueba", "Empresa test", "Enaex DEMO", "Stark Industries", "Customer Success Demos", "Llevepan","BBVAdemo","BCP Demo","Clientes Rankmi","Co op","Empresas ACME", "Fortuna Silver Mines", "Fortuna Silver Mines 2","Manuelita", "Only app", "prueba", "Red", "Rankmi Test", "Sales Manager","test creacion","Test intercom 1","test org chart","tyrell","Tyrell Corp.")
+  output <- sort(enterprises)
+  return(output)
+}
