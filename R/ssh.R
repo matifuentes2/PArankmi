16c31f8c7e5727f0634c163aedd6a2a24b23805a
matifuentes2 <matifuentes2@gmail.com>
2020-11-18 12:29:23 -0300

    update ssh funct

--- R/ssh.R
+++ R/ssh.R
@@ -7,7 +7,10 @@
 ssh <- function(location = "/home/matias/Documents/Credentials/rankmi_office.pem",
                 port = "5556"){
 
-  system(glue::glue("ssh -f -i {location} -L {port}:rankmi-prod-cluster.cluster-ro-crtmr3w7usfm.us-west-2.rds.amazonaws.com:5432 -p 2233 ubuntu@54.148.6.196 -N "))
+  if ((system("ps -fC ssh", intern = T) %>% length()) == 1){
+    system(glue::glue("ssh -f -i {location} -L {port}:rankmi-prod-cluster.cluster-ro-crtmr3w7usfm.us-west-2.rds.amazonaws.com:5432 -p 2233 ubuntu@54.148.6.196 -N "))
+  }
+
 }
 
 
