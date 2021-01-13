c658964c9ca684c026bcd03c4e2cb7cee079ee84
Matias Fuentes <matifuentes2@gmail.com>
2021-01-13 13:28:31 -0300

    Actualizo ssh para acceder desde windows

--- R/ssh.R
+++ R/ssh.R
@@ -4,14 +4,25 @@
 #' @keywords ssh
 #' @export
 
-ssh <- function(location = "/home/matias/Documents/Credentials/rankmi_office.pem",
+ssh <- function(os = "linux",
+                location = "/home/matias/Documents/Credentials/rankmi_office.pem",
                 port = "5556"){
 
-  if ((system("ps -fC ssh", intern = T) %>% length()) == 1){
-    system(glue::glue("ssh -f -i {location} -L {port}:rankmi-prod-cluster.cluster-ro-crtmr3w7usfm.us-west-2.rds.amazonaws.com:5432 -p 2233 ubuntu@54.148.6.196 -N "))
+  if(os == "windows"){
+
+    shell(glue::glue("START ssh -i C:/Users/matif/OneDrive/Documents/Credentials/rankmi_office.pem -L 5556:rankmi-prod-cluster.cluster-ro-crtmr3w7usfm.us-west-2.rds.amazonaws.com:5432 -p 2233 ubuntu@54.148.6.196"))
+
+  }
+
+
+  if(os == "linux"){
+    if ((system("ps -fC ssh", intern = T) %>% length()) == 1){
+      system(glue::glue("ssh -f -i {location} -L {port}:rankmi-prod-cluster.cluster-ro-crtmr3w7usfm.us-west-2.rds.amazonaws.com:5432 -p 2233 ubuntu@54.148.6.196 -N "))
+    }
   }
 
 }
+
 
 
 
